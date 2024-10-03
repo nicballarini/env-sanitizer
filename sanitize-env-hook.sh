aa97c252a81bd667b1e2fc4551b17526072514c4
nicballarini <me@alar.io>
2024-10-02 23:47:24 -0600

    Handle .EXAMPLE naming, rename lowercase .example, support port variations

--- sanitize-env-hook.sh
+++ sanitize-env-hook.sh
@@ -3,19 +3,19 @@
 # Function to process .env files
 process_env_file() {
   env_file="$1"
-  example_file="$env_file.example"
+  example_file="${env_file%.example}.EXAMPLE"  # Ensure the copied file ends in .EXAMPLE
 
-  # Copy the .env file to .env.example
+  # Copy the .env file to .EXAMPLE
   cp "$env_file" "$example_file"
 
   # Check OS type and adjust sed syntax accordingly
   if [[ "$OSTYPE" == "darwin"* ]]; then
     # macOS: Use sed with an empty '' argument for in-place editing
-    sed -i '' '/^[^#]*port=/! s/=.*/=/' "$example_file"
+    sed -i '' '/^[^#]*[Pp][Oo][Rr][Tt]=/! s/=.*/=/' "$example_file"
     sed -i '' '1s/^/# generated automatically by .git\/hooks\/pre-commit\n/' "$example_file"
   else
     # Linux: Use sed without an argument for in-place editing
-    sed -i '/^[^#]*port=/! s/=.*/=/' "$example_file"
+    sed -i '/^[^#]*[Pp][Oo][Rr][Tt]=/! s/=.*/=/' "$example_file"
     sed -i '1s/^/# generated automatically by .git\/hooks\/pre-commit\n/' "$example_file"
   fi
 
@@ -24,11 +24,16 @@ process_env_file() {
     echo "$env_file" >> .gitignore
   fi
 
-  # Stage the .env.example file for the commit
+  # If an env.*.example (lowercase) exists, rename it to uppercase
+  if [[ "$env_file" == *".example" ]]; then
+    mv "$env_file" "${env_file%.example}.EXAMPLE"
+  fi
+
+  # Stage the .EXAMPLE file for the commit
   git add "$example_file"
 }
 
-# Find all .env files including .env.<environment>, excluding .example files
-find . -type f -name ".env*" ! -name "*.example" | while read -r env_file; do
+# Find all .env files including .env.<environment>, excluding .EXAMPLE files
+find . -type f -name ".env*" ! -name "*.EXAMPLE" | while read -r env_file; do
   process_env_file "$env_file"
 done
